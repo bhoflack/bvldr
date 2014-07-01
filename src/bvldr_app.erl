b7067b9629832c4a41790247b9601089e5db273d
Brecht Hoflack <brecht.hoflack@gmail.com>
2014-07-01 20:59:44 +0200

    Initial commit

---
+++ src/bvldr_app.erl
@@ -0,0 +1,16 @@
+-module(bvldr_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    bvldr_sup:start_link().
+
+stop(_State) ->
+    ok.
